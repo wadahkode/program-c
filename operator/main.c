c2d1d3a755908a4a5c87b3e020ba5a867c9ca828
wadahkode <mvp.dedefilaras@gmail.com>
2021-04-14 10:57:17 +0700

    operator

---
+++ operator/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  int a = 2;
+  int b = 10;
+
+  printf("-------------------------------\n");
+  printf("Operator Artimatika dan penugasan\n");
+  printf("-------------------------------\n");
+  printf("Jika a = 2 dan b = 10\n");
+  printf("-------------------------------\n");
+  printf("Penambahan: %d\n", a + b);
+  printf("Pengurangan: %d\n", b - a);
+  printf("Perkalian: %d\n", a * b);
+  printf("Pembagian: %d\n", b / a);
+  printf("Sisa bagi: %d\n", b % a);
+  printf("-------------------------------\n");
+
+  return 0;
+}
